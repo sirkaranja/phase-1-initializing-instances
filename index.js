d76e458bda98cc50104284ff88ede3503f9c2995
Daniel Karanja <sirkaranja12@gmail.com>
2023-04-01 10:28:39 +0300

    classes

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your code here
+class Breakfast{
+    #food;
+    constructor(food,drink){
+        this.#food= food;
+        this.drink=drink;
+    }
+}
+class Lunch{
+     constructor(salad,soup,drink){
+        this.salad=salad;
+        this.drink=drink;
+        this.soup=soup;
+    }
+
+}
+
+class Dinner{
+    #dessert;
+    constructor(dessert,salad,soup,entree){
+        this.#dessert=dessert;
+        this.salad=salad;
+        this.soup=soup;
+        this.entree=entree;
+    }
+}
